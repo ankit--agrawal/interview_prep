53b72ee943e6daf8678ce540a0f57e2f81db22e1
Ankit Agrawal <ankit@cs.utah.edu>
2017-09-01 17:56:24 -0600

    updates

--- sort/insertion/Insertion_Sort.c
+++ sort/insertion/Insertion_Sort.c
@@ -1,48 +1,40 @@
 #include"stdio.h"
 #include"stdlib.h"
 
-main()
+int main()
 {
-	int loop = 0, n , i, j, min, min_loc;
-	int *Sort_Array;
-	int temp = 0;
-	printf("Enter the size of the array");
-	scanf("%d", &n);
-	Sort_Array = (int *)malloc(sizeof(int)*n);
+int n,i=0,j=0,k=0,min=0, size=0,location=0;
+int *sort_array;
+printf("Enter the size of array= ");
+scanf("%d",&n);
+sort_array = (int *)malloc(sizeof(int)*n);
 
-	printf("Enter the elements of the array\n");
-	for(loop = 0; loop < n; loop++)
-	{
-		printf("Enter element number %d = ",loop);
-		scanf("%d", &Sort_Array[loop]);
+	for (i=0;i<n;i++){
+		printf("Enter element number %d =",i);
+		scanf("\n%d",&sort_array[i]);
 	}
 
-
-	for(i = 0; i < n-1; i++)
-	{
-		min = Sort_Array[i];
-		min_loc = i;
-		j = i + 1;
-		while(j < n)
-		{
-			if(Sort_Array[j] < min)
-			{
-				min = Sort_Array[j];
-				min_loc = j;
+	while (size != n - 1 ){
+		for (i=size;i<n - 1;i++){
+			min = sort_array[i];
+			location = i;
+			for (j=i+1;j<n;j++){
+				if (sort_array[j]<min){					
+					min = sort_array[j];
+					location = j;
+				}
 			}
-			j++;
-		}
-		for(j = min_loc; j > i; j--)
-		{
-			temp = Sort_Array[j];
-			Sort_Array[j] = Sort_Array[j-1];
-			Sort_Array[j-1] = temp;
+			//printf("Minimum for element %d is %d\n", size, min);
+			for (k=location-1;k>=size;k--){
+				sort_array[k+1] = sort_array[k];
+			}
+			sort_array[size] = min;
+			size = size+1;
 		}
 	}
-
-	printf("After Insertion\n");
-	for(loop = 0; loop < n; loop++)
-	{
-		printf("Element at value %d is %d\n",loop,Sort_Array[loop]);
+	printf("Sorting array...\n");
+	for(i=0;i<n;i++){
+		printf("Value at location %d is %d\n",i,sort_array[i]);
 	}
 }
+			
